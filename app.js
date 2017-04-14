11fbd5835ef044d55bb0c3672d85c16a17fe83a1
Juan Meza <jrmeza514@gmail.com>
2017-04-13 19:05:22 -0700

    Commit
    More Work

--- app.js
+++ app.js
@@ -9,18 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use( express.static( __dirname + "/dist/") );
 
-let batchQueue = [{
-  name: "ITM-BB01-0",
-  DPCIS: 20,
-  eaches: 480,
-  owner: "Juan Meza"
-},
-{
-  name: "ITM-FURN-0",
-  DPCIS: 3,
-  eaches: 6,
-  owner: "Juan Meza"
-}];
+let batchQueue = [{name: "ITM-BB01", DPCIS: 5, eaches: 17, owner: "Juan Meza"}];
 const maxBatches = 20;
 const maxBatchAge = 2000;
 
@@ -28,12 +17,18 @@ socket.on('connection', (client) => {
   client.emit('connected', batchQueue);
 
   client.on('newBatch', (batchOptions) => {
-    batchQueue.push(new Batch(batchOptions));
-    client.emit('batchReceived', batchOptions);
+    batchQueue.push(batchOptions);
+    client.emit('newBatch', batchOptions);
+    client.broadcast.emit('newBatch', batchOptions);
   });
 
   client.on('batchComplete', (batchOptions) => {
-
+    client.emit('batchComplete', batchOptions);
+    client.broadcast.emit('batchComplete', batchOptions);
+  });
+  client.on('batchCancelled', (batchOptions) => {
+    client.emit('batchCancelled', batchOptions);
+    client.broadcast.emit('batchCancelled', batchOptions);
   });
 
 });
--- dev/js/client.js
+++ dev/js/client.js
@@ -1,9 +1,32 @@
 let BatchQueue = ( () => {
-  /*
-    CHAT_MESSAGES is the local cache for the messages broadcasted by the server.
-    There is no limit specified for local chat buffer
-  */
-  let CHAT_MESSAGES = [];
+  const addBatchFAB = document.getElementById('newBatchButton');
+  const BATCH_LIST = document.getElementById('batchList');
+  const newBatchNameInput = document.getElementById('newBatchName');
+  const newBatchDPCISInput = document.getElementById('newBatchDPCIS');
+  const newBatchEachesInput = document.getElementById('newBatchEaches');
+  const newBatchOwnerInput = document.getElementById('newBatchOwner');
+  const submitNewBatchButton = document.getElementById('submitNewBatchButton');
+  const newBatchForm = document.getElementById('newBatchForm');
+  const cancelNewBatchButton = document.getElementById('cancelNewBatchButton');
+
+
+  let addBatchOverlayOpen = false;
+
+  let toggleAddBatchOverlay = () => {
+    if (addBatchOverlayOpen) {
+      newBatchForm.style.left = "100%";
+    }else{
+      newBatchForm.style.left = "0px";
+    }
+    newBatchNameInput.value = "";
+    newBatchDPCISInput.value = "";
+    newBatchEachesInput.value = "";
+    newBatchOwnerInput.value = "";
+    addBatchOverlayOpen = !addBatchOverlayOpen;
+
+  };
+  addBatchFAB.addEventListener('click', toggleAddBatchOverlay , false);
+  cancelNewBatchButton.addEventListener('click', toggleAddBatchOverlay, false);
 
   /*
     Specify the URI for the server that is hosting the chat
@@ -22,18 +45,23 @@ let BatchQueue = ( () => {
   let socket = null;
   let SOCKET_CALLBACKS = {};
 
+  let batchQueueBuffer = [];
   /*
     SOCKET_CALLBACKS.onConnected receives confirmation that we have successfully
     connected to the chat server and sends back the username we wish the server
     to use
   */
   SOCKET_CALLBACKS.onConnected =  (batchQueue) => {
-    let batchList = document.getElementById('batchList');
+    batchQueueBuffer = batchQueue;
     for (var i = 0; i < batchQueue.length; i++) {
       var batch = batchQueue[i];
-      //batchList.innerHTML += "Name: " + batch.name + " DPCIS: " + batch.DPCIS + " Eaches: " + batch.eaches + " Owner: " + batch.owner + " <br/>";
-
+      batchList.appendChild(createBatchUI(batch));
     }
+  };
+
+  SOCKET_CALLBACKS.onBatchAdded = (batchOptions) => {
+    batchQueueBuffer.push(batchOptions);
+    BATCH_LIST.appendChild(createBatchUI(batchOptions));
   };
 
 
@@ -56,8 +84,82 @@ let BatchQueue = ( () => {
       Add the event listeners to the cosket
     */
     socket.on( 'connected', SOCKET_CALLBACKS.onConnected );
+    socket.on('newBatch', SOCKET_CALLBACKS.onBatchAdded );
 
   };
+
+  let addBatch = () => {
+    if(!socket) return;
+
+    let values = {
+      name: newBatchNameInput.value,
+      DPCIS: newBatchDPCISInput.value,
+      eaches: newBatchEachesInput.value,
+      owner: newBatchOwnerInput.value
+    };
+
+    let areYouSure = confirm("Are you sure you want to submit " + values.name + "?");
+    if(!areYouSure) return;
+
+    socket.emit('newBatch', values);
+
+  };
+
+
+  submitNewBatchButton.addEventListener('click', (e) => {
+    addBatch();
+    toggleAddBatchOverlay();
+  }, false);
+let createBatchUI = (batchOptions) => {
+  let batch = document.createElement('div');
+  batch.className = "batch";
+
+  let batchName = document.createElement('div');
+  batchName.className = "batchName";
+  batchName.innerText = batchOptions.name;
+
+  batch.appendChild(batchName);
+
+  let batchStats = document.createElement("div");
+  batchStats.className = "batchStats";
+
+  let batchDPCIS = document.createElement("div");
+  batchDPCIS.className = "batchDPCIS";
+  batchDPCIS.innerText = "DPCIs: " + batchOptions.DPCIS;
+
+  let batchEaches = document.createElement("div");
+  batchEaches.className = "batchEaches";
+  batchEaches.innerText = "Eaches: " + batchOptions.eaches;
+
+  batchStats.appendChild(batchDPCIS);
+  batchStats.appendChild(batchEaches);
+
+  batch.appendChild(batchStats);
+
+  let batchOwner = document.createElement("div");
+  batchOwner.className = "batchOwner";
+  batchOwner.innerText = "Owner: " + batchOptions.owner;
+
+  batch.appendChild(batchOwner);
+
+  let controls = document.createElement("div");
+  controls.className = "controls";
+
+  let completeButton = document.createElement("div");
+  completeButton.className = "completeButton";
+  completeButton.innerText = "Complete";
+
+  let cancelButton = document.createElement("div");
+  cancelButton.className = "cancelButton";
+  cancelButton.innerText = "Cancel";
+
+  controls.appendChild(cancelButton);
+  controls.appendChild(completeButton);
+
+  batch.appendChild(controls);
+
+  return batch;
+};
 
   return {
     connect: connect
